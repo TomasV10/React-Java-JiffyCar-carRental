6d3dea467424db076d0e5d4f843f1ab9d9703534
tomasv10 <tomasvarnauskas19@gmail.com>
2020-09-03 13:38:46 +0300

    Crud operations created

--- src/main/java/com/practise/JiffyCar/controllers/CarsController.java
+++ src/main/java/com/practise/JiffyCar/controllers/CarsController.java
@@ -3,22 +3,40 @@ package com.practise.JiffyCar.controllers;
 import com.practise.JiffyCar.entities.Car;
 import com.practise.JiffyCar.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cars")
+@RequestMapping("/api")
 @CrossOrigin("*")
 public class CarsController {
     @Autowired
     private CarService carService;
 
-    @GetMapping
+    @GetMapping("/cars")
     public List<Car> getCarList(){
         return carService.getCarsList();
+    }
+    @GetMapping("/cars/{id}")
+    public Car get(@PathVariable Integer id) {
+        return carService.get(id);
+    }
+
+    @PostMapping("/cars")
+    public Car addCar(@RequestBody Car car) {
+        return carService.save(car);
+    }
+
+    @PutMapping("/cars/{id}")
+    public void update(@RequestBody  Car car,
+                       @PathVariable Integer id) {
+        Car existingCar = carService.get(id);
+        carService.save(car);
+    }
+
+    @DeleteMapping("/cars/{id}")
+    public void delete (@PathVariable Integer id) {
+       carService.delete(id);
     }
 }
