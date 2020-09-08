d093472becee7925b5204ead2afa95594125d494
tomasv10 <tomasvarnauskas19@gmail.com>
2020-09-08 13:06:05 +0300

    Initialized storage

--- src/main/java/com/practise/JiffyCar/JiffyCarApplication.java
+++ src/main/java/com/practise/JiffyCar/JiffyCarApplication.java
@@ -1,13 +1,26 @@
 package com.practise.JiffyCar;
 
+import com.practise.JiffyCar.services.FileStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class JiffyCarApplication {
+public class JiffyCarApplication implements CommandLineRunner {
+
+	@Resource
+	FileStorageService fileStorageService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JiffyCarApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		fileStorageService.deleteAll();
+		fileStorageService.init();
+	}
 }
